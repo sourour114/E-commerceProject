de9cab2be16afd5bdf88fe1471114979d0488928
sourour <souroursiala355@gmail.com>
2021-08-04 10:00:28 +0100

    add Movie model , MoviesController , Random.cshtml(view)

---
+++ ECommerce/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerce.Models;
+
+namespace ECommerce.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name="shrek!"};
+            return View(movie);
+        }
+    }
+}
